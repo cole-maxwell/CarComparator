ecbb64b3f9acde4e2c3ae63f2c6531e9c807af04
Cole Maxwell <cole.h.maxwell@gmail.com>
2020-12-21 22:03:18 -0500

    small update

--- src/edu/umb/cs680/hw11/MileageComparator.java
+++ src/edu/umb/cs680/hw11/MileageComparator.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 public class MileageComparator implements Comparator<Car>
 {
 	public int compare(Car c1, Car c2) {
-		return (c1.getYear() - c2.getYear());
+		return (c1.getMileage() - c2.getMileage());
 	}
 }
 
